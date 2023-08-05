76ba99fffbc8716bce7b6f52ea65eec279ae2440
rashi ghosh <rashighosh@ufl.edu>
2023-08-05 15:45:35 -0400

    added link to trials and copy email text

--- public/scripts/studySearch.js
+++ public/scripts/studySearch.js
@@ -22,6 +22,29 @@ if(window.location.toString().indexOf("Results") != -1){
     console.log(JSON.parse(sessionStorage.getItem('preferences-info')))
 }
 
+function copyEmailText(contactName, studyTitle, briefSummary) {
+    console.log("IN COPY EMAIL TEXT")
+    console.log(contactName)
+    console.log(briefSummary)
+
+    let text= `Hi ${contactName},
+
+    My name is [YOUR NAME HERE]. I saw your study ${studyTitle} through the Research Studies section on the ALEX site. I’m interested in participating and would like more information. Here’s the description of your study that I read:
+    
+    ${briefSummary}
+    
+    
+    Thank you,
+    
+    [YOUR NAME]
+    
+    [YOUR EMAIL]`
+
+    console.log(text)
+
+    alert("The following text was successfully copied!\n\n" + text)
+}
+
 async function getResults() {
     console.log("GOING TO SEARCH FOR CT")
 
