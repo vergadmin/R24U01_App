fea61963450f210082d160cf5ce5e92ff68c9d32
rashi ghosh <rashighosh@ufl.edu>
2023-08-16 09:37:46 -0400

    removed proxy

--- server.js
+++ server.js
@@ -49,7 +49,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     rolling: true,
-    proxy: true,
     cookie: {
         maxAge: 1000 * 60 * 15,
         secure: true, // switch to true
