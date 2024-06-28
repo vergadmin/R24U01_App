309a4f8f6fee676aa0f375ab1d8f2937e6b49812
rghosh96 <rashighosh96@gmail.com>
2024-06-28 12:30:40 -0400

    more patient v caregiver language

--- public/scripts/StudySearch/checkRole.js
+++ public/scripts/StudySearch/checkRole.js
@@ -5,9 +5,17 @@ var role = sessionStorage.getItem('Role')
 console.log(role)
 if (document.getElementById("background-info")) {
     console.log("BACKGROUND INFO EXISTS")
-    if (role === 'Caregiver') {
+    if (role === 'Patient') {
         document.getElementById('background-description').innerHTML = `Please fill out the information below so we can find research studies <b>you might qualify for</b>.`
     } else {
         document.getElementById('background-description').innerHTML = `Please fill out the information below so we can find research studies <b>the person you're entering information for might qualify for</b>.`
+    }
+}
+
+if (document.getElementById("preferences-description")) {
+    if (role === 'Patient') {
+        document.getElementById('preferences-description').innerHTML = `Please fill out the information below so we can find research studies <b>you might qualify for</b>.`
+    } else {
+        document.getElementById('preferences-description').innerHTML = `Please fill out the information below so we can find research studies <b>the person you're entering information for might qualify for</b>.`
     }
 }
